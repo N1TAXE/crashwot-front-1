fd2107904d2863b984745fa1c2fbec120f26f849
N1TAXE <n1t4x3@gmail.com>
2024-06-17 21:07:29 +0200

    new crashgame animation

--- src/pages/home/ui/crashgame-animation/index.tsx
+++ src/pages/home/ui/crashgame-animation/index.tsx
@@ -3,27 +3,31 @@ import {Icon} from "../../../../shared/ui/icon";
 import {useAnimation} from "../../../../shared/lib/utils";
 import {MutableRefObject, useEffect, useRef} from "react";
 export const CrashGameAnimation = () => {
-    const tankRef = useRef<HTMLElement>(null)
-    const [tank] = useAnimation({
-        ref: tankRef,
-        name: "tank",
-        viewport: {
-            x: -608,
-            y: 0,
-            width: 608 * 2,
-            height: 304 * 2,
-        },
-    });
-    useEffect(() => {
-        if (tank) {
-            console.log(tank)
+    const ctRef = useRef<HTMLElement>(null)
+    const [ct] = useAnimation({
+        ref: ctRef,
+        name: "CSGO",
+        config: {
+            preserveDrawingBuffer: false,
+            viewport: {
+                x: -(1080 / 2),
+                y: 0,
+                width: 1080,
+                height: 1080,
+            },
+            animation: "start"
         }
-    }, [tank]);
+    });
+
+    const setAnim = () => {
+        ct?.setAnimation("Run")
+        console.log(ct)
+    }
     return (
         <div className={styles.card}>
             <div className={styles.info}>
                 <div className={styles.timer}>
-                    <span>
+                    <span onClick={setAnim}>
                         13.53 сек
                     </span>
                     До следующей игры
@@ -39,7 +43,7 @@ export const CrashGameAnimation = () => {
                     </span>
                 </div>
             </div>
-            <div ref={tankRef as MutableRefObject<HTMLDivElement>} className={styles.animation}></div>
+            <div ref={ctRef as MutableRefObject<HTMLDivElement>} className={styles.animation}></div>
             <div className={styles.road}></div>
         </div>
     )
--- src/shared/lib/utils/index.tsx
+++ src/shared/lib/utils/index.tsx
@@ -2,44 +2,32 @@ import {MutableRefObject, RefObject, useEffect, useState} from "react";
 
 import { SpinePlayer } from "@esotericsoftware/spine-player";
 import {StringMap} from "@esotericsoftware/spine-core";
-import {Viewport} from "@esotericsoftware/spine-player/dist/Player";
+import {SpinePlayerConfig, Viewport} from "@esotericsoftware/spine-player/dist/Player";
 
 export interface AnimationTypes {
     ref: MutableRefObject<HTMLElement> | RefObject<HTMLElement>;
     name: string;
-    viewport: {
-        x?: number;
-        y?: number;
-        width?: number;
-        height?: number;
-        padLeft?: string | number;
-        padRight?: string | number;
-        padTop?: string | number;
-        padBottom?: string | number;
-        debugRender?: boolean;
-        transitionTime?: number;
-        animations?: StringMap<Viewport>;
-    }
+    config: SpinePlayerConfig
 }
 
-export const useAnimation = ({ ref, name, viewport = {}, ...rest }: AnimationTypes) => {
+export const useAnimation = ({ ref, name, config }: AnimationTypes) => {
     const [animation, setAnimation] = useState<SpinePlayer>();
 
     useEffect(() => {
         if (!ref?.current) return;
 
-        const spine = new SpinePlayer(ref?.current, {
-            preserveDrawingBuffer: false,
+        const spine = new SpinePlayer(ref.current, {
             jsonUrl: `/static/animation/${name}/${name}.json`,
             atlasUrl: `/static/animation/${name}/${name}.atlas`,
             backgroundColor: "#00000000",
             alpha: true,
             showControls: false,
             showLoading: false,
-            animation: "idle",
             defaultMix: 0.25,
-            viewport,
-            ...rest,
+            viewport: {
+                ...config.viewport
+            },
+            ...config
         });
 
         setAnimation(spine);
